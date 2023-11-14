36cc83df75a60dbfa8712ef1f199a1fab33adad0
wommiez <149967204+wommiez@users.noreply.github.com>
2023-11-14 21:41:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,113 @@
-let words = ["PIANO","HAND","COSMETIC"];
+let words = [
+    "PILAGE",
+    "INNOCUOUS",
+    "COSMETIC", 
+    "FANTASTIC", 
+    "LUSCIOUS",
+    "SINISTER",
+    "PALEOLITIC",
+    "DISCERN",
+    "WONDROUS",
+    "CLAMMY",
+    "RUSTLE",
+    "CAMOUFLAGE",
+    "KISMET",
+    "BARBARIC",
+    "VENTRILOQUIST",
+    "DANDELION",
+    "WRETCHED",
+    "HARBINGER",
+    "ZEALOUS",
+    "EARTHBOUND"
+];
 var game = {};
 
 function new_game(){
-    game = new_word();
-    game.lives = 8;
-    show_word(game.charactersToGuess);
-    show_word2(game.charactersWord);
-    show_image();
+game = new_word();
+game.lives = 9;
+show_word(game.charactersToGuess);
+show_incorrect(game.charactersUsed);
+show_image();
 }
 
 function new_word() {
-    var i = random_number(words.length);
-    var word = words[i];
-    var charactersToGuess = Array(word.length).fill('_');
-    var charactersWord = Array(word.length);
-    var charactersUsed = Array();
-    for(let i = 0; i < word.length; i++){
-        charactersWord[i] = word.charAt(i);
-    }
-    let number_gaps = word.length;
-    return {word: word, charactersToGuess: charactersToGuess, number_gaps: number_gaps, charactersWord: charactersWord, charactersUsed: charactersUsed};
+var i = random_number(words.length);
+var word = words[i];
+var charactersToGuess = Array(word.length).fill('_');
+var charactersWord = Array(word.length);
+var charactersUsed = Array();
+for(let i = 0; i < word.length; i++){
+charactersWord[i] = word.charAt(i);
+}
+let number_gaps = word.length;
+return {word: word, charactersToGuess: charactersToGuess, number_gaps: number_gaps, charactersWord: charactersWord, charactersUsed: charactersUsed};
 }
 
 function random_number(max){
-    return Math.floor(Math.random() * max)
+return Math.floor(Math.random() * max)
 }
 
 function show_word(character){
-    var element = document.getElementById('gaps');
-    element.textContent = character.join(' ');
+var element = document.getElementById('gaps');
+element.textContent = character.join(' ');
 }
 
-function show_word2(character){
-    var element = document.getElementById('wordtoguess');
-    element.textContent = character.join(' ');
+function show_incorrect(character){
+var element = document.getElementById('incorrect_letters');
+element.textContent = character.join(' ');
 }
 
 function show_image(){
-    var element = document.getElementById('hangman_image');
-    element.src = "img/hangman_"+game.lives+".png";
+var element = document.getElementById('hangman_image');
+element.src = "img/hangman_"+game.lives+".png";
 }
 
 function letter_send(letter){
-    var letterInserted = letter.value.toUpperCase();
-    letter.value = "";
-    if(/^[A-Z]$/.test(letterInserted)) {
-        letter.value = "";
-        let repeated = false;
-        let letterInWord = false;
-        for(let i = 0; i < game.charactersUsed.length; i++){
-            if(letterInserted === game.charactersUsed[i]){
-                repeated = true;
-                break;
-            }
-        }
-        if(repeated === false){
-            for(let i = 0; i < game.charactersWord.length; i++){
-                if(letterInserted === game.charactersWord[i]){
-                    letterInWord = true;
-                    game.charactersToGuess[i] = letterInserted;
-                    game.number_gaps = game.number_gaps-1;
-                }
-            }
-            game.charactersUsed.push(letterInserted);
-            if(game.number_gaps === 0){
-                game_win();
-            }
-        }
-        if(letterInWord === false){
-            game.lives--;
-            var element = document.getElementById('hangman_image');
-            element.src = "img/hangman_"+game.lives+".png";
-            if(game.lives === 0){
-                game_loss();
-            }
-        }
-        show_word(game.charactersToGuess);
-        console.log("Letras restantes = "+game.number_gaps);
-        console.log("Numero Vidas = "+game.lives);
+var letterInserted = letter.value.toUpperCase();
+letter.value = "";
+if(/^[A-Z]$/.test(letterInserted)) {
+letter.value = "";
+let repeated = false;
+let letterInWord = false;
+for(let i = 0; i < game.charactersUsed.length; i++){
+    if(letterInserted === game.charactersUsed[i]){
+        repeated = true;
+        break;
     }
+}
+if(repeated === false){
+    for(let i = 0; i < game.charactersWord.length; i++){
+        if(letterInserted === game.charactersWord[i]){
+            letterInWord = true;
+            game.charactersToGuess[i] = letterInserted;
+            game.number_gaps = game.number_gaps-1;
+        }
+    }
+    game.charactersUsed.push(letterInserted);
+    if(game.number_gaps === 0){
+        game_win();
+    }
+}
+if(letterInWord === false){
+    game.lives--;
+    var element = document.getElementById('hangman_image');
+    element.src = "img/hangman_"+game.lives+".png";
+    if(game.lives === 0){
+        game_loss();
+    }
+}
+show_word(game.charactersToGuess);
+show_incorrect(game.charactersUsed);
+console.log("Letras restantes = "+game.number_gaps);
+console.log("Numero Vidas = "+game.lives);
+}
 }
 
 function game_loss() {
-    alert("Perdiste");
+alert("You lost!\n The word was: "+game.word);
 }
 
 function game_win() {
-    alert("Ganaste")
+alert("You won!")
 }
+
